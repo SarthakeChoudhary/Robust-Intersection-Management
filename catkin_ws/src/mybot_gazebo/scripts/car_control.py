146c5b055f611b1647be8d17a4f17363779b4f5e
SarthakeChoudhary <sarthak.choudhary24@gmail.com>
2019-03-27 14:45:17 -0700

    Working planner

--- catkin_ws/src/mybot_gazebo/scripts/car_control.py
+++ catkin_ws/src/mybot_gazebo/scripts/car_control.py
@@ -6,24 +6,71 @@ from sensor_msgs.msg import LaserScan
 import geometry_msgs.msg
 from nav_msgs.msg import Odometry
 
-vel= 0.2
+velocity= 0.2
+position = 0
+flag = 0
+id = 1
+emergency = 0
+j = 0
+rcv_im = [None] * 2
+
+def send_data_to_IM():
+    global emergency
+    pub_im = rospy.Publisher('/comm2', std_msgs.msg.UInt8, queue_size=10)
+    pub_im.publish(65)
+    if emergency == 0:
+        value = (128 + int(velocity*10))
+        pub_im.publish(value)
+        pub_im.publish(position)
+    else:
+        value = (192 + 3)       #11....11 control packet to indicate obstruction in path
+        pub_im.publish(value)
+        emergency = 2
+
 
 def callback_im(msg):
-    print(msg)
-    global vel
-    vel = int(msg)
+    # print((msg.data))
+    global velocity, rcv_im, j, flag
+    if (j < 2):
+        rcv_im[j] = msg.data
+        j = j + 1
+
+    if (j == 1 and rcv_im[0] >> 6 != 1):
+        j=0
+        if flag == 0:
+            send_data_to_IM()
+
+    if (j==2):
+        packet_type = (rcv_im[1] >> 6)
+        curr_id = rcv_im[0] & 63
+        print(curr_id)
+        print(packet_type)
+        if (curr_id == id):
+            if (packet_type == 2):
+                velocity = (rcv_im[1] & 63)/ 10.0
+                print('voa received', velocity)
+                flag = 1                        #to indicate that voa has been received
+            elif (packet_type >> 6 == 3):
+                print('error')
+        j=0
+
+
 
 def callback_odom(msg):
     # print(msg.pose.pose.position.x)
-    if (msg.pose.pose.position.x >= -2):
-        pub_im.publish(64)
+    global flag, position
+    position = msg.pose.pose.position.x
+    if (position >= 2 and flag == 0):
+        send_data_to_IM()
 
 def callback_laser(msg):
     # print((msg.ranges[354:366]))
-    global vel
-    for i in msg.ranges[356:365]:
-        if (i < 0.5):
-            vel = 0
+    global velocity, emergency
+    for i in msg.ranges[350:370]:
+        if (i < 1.5 and emergency != 2):
+            emergency = 1
+            send_data_to_IM()
+            velocity = 0
             break
 
 
@@ -31,13 +78,13 @@ if __name__ == '__main__':
 
     rospy.init_node('Car_Controller')
     pub_car_vel = rospy.Publisher('/robot1/cmd_vel', geometry_msgs.msg.Twist, queue_size=1)
-    pub_im = rospy.Publisher('/comm', std_msgs.msg.Int8, queue_size=1)
-    # sub_im = rospy.Subscriber('/comm', std_msgs.msg.Int8, callback_im)
+    pub_im = rospy.Publisher('/comm2', std_msgs.msg.UInt8, queue_size=10)
+    sub_im = rospy.Subscriber('/comm1', std_msgs.msg.UInt8, callback_im)
     sub_odom = rospy.Subscriber('/robot1/odom', Odometry, callback_odom)
     sub_laser = rospy.Subscriber('/mybot/laser/scan', LaserScan, callback_laser)
     cmd = geometry_msgs.msg.Twist()
     while not rospy.is_shutdown():
-        cmd.linear.x = vel
+        cmd.linear.x = velocity
         # print(cmd.linear.x)
         pub_car_vel.publish(cmd)
 
--- catkin_ws/src/mybot_gazebo/scripts/im.py
+++ catkin_ws/src/mybot_gazebo/scripts/im.py
@@ -4,20 +4,69 @@ import std_msgs.msg
 import math
 
 flag = 0
+id = [0] * 64           #list of all the IDs
+check = 0
+j = 0
+rcv_car = [None] * 3
+velocity = [None] * 64
+position = [None] * 64
+curr_id= 70           #random value greater than 64
+voa = [None] *64
+voa[1] = 1
+id[1] = 1
+emergency = False
+
+def send_data_to_car():
+
+    if emergency == False:
+        pub = rospy.Publisher('/comm1', std_msgs.msg.UInt8, queue_size=10)
+        pub.publish(65)
+        pub.publish(128 + (voa[curr_id] * 10.0))
+
+    else:
+        pub = rospy.Publisher('/comm2', std_msgs.msg.UInt8, queue_size=10)
+        pub.publish(66)
+        pub.publish(128 + (voa[curr_id] * 10.0))
+
 
 def callback(msg):
-    global flag
-    print(msg)
-    if(msg == std_msgs.msg.Int8(64)):
-        print('hello')
-        flag = 1
+    global flag, check, rcv_car,curr_id, emergency
+    global j
+    i = 3
+
+    if j < i:
+        rcv_car[j] = msg.data
+        j = j + 1
+    if j == 1 and rcv_car[0] >> 6 != 1:
+        j = 0
+
+    if j == 2:
+        packet_type = rcv_car[1] >> 6
+        if packet_type == 3:
+             i = 2
+
+    if j == i:
+        packet_type = rcv_car[1] >> 6
+        curr_id = (rcv_car[0] & 63)
+        if (id[curr_id] == 1):
+            if(packet_type == 2):
+                velocity[curr_id] = (rcv_car[1] & 63)/10.0
+                position[curr_id] = rcv_car[2]
+                print("V-info received from car")
+                print('velocity = ', velocity[curr_id])
+                print('Position = ', position[curr_id])
+                send_data_to_car()
+            elif (packet_type == 3):
+                value = rcv_car[1] & 63
+                if(value == 3):
+                    print('Emergency Detected \nRescheduling')
+                    emergency = True
+                    send_data_to_car()
+
+        j=0
+
 
 if __name__ == '__main__':
-    global flag
     rospy.init_node('IM')
-    pub = rospy.Publisher('/comm', std_msgs.msg.Int8,queue_size=1)
-    sub = rospy.Subscriber('/comm', std_msgs.msg.Int8, callback)
-    while not rospy.is_shutdown():
-        if flag == 1:
-            pub.publish(2)
+    sub = rospy.Subscriber('/comm2', std_msgs.msg.UInt8, callback)
     rospy.spin()
